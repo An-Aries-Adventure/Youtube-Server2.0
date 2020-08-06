6d07d48104f1269e94e53c27cb44473c662e770c
unknown <kylespinaio@hotmail.com>
2020-08-05 20:32:03 -0400

    added all crud functions tested with postman.

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -46,5 +46,44 @@ router.post('/', async (req, res) => {
 }
 })
 
+router.put('/:id', async (req, res) => {
+    try {
+        const { error } = validate(req.body);
+        if (error) return res.status(400).send(error);
+
+    const video = await Video.findByIdAndUpdate(
+    req.params.id,
+    {
+        videoId: req.body.videoId,
+        likes: req.body.likes,
+        dislikes: req.body.dislikes,
+        comments: []
+    },
+    { new: true }
+    );
+    if (!video)
+    return res.status(400).send(`The video with id "${req.params.id}" d
+   oes not exist.`);
+    await video.save();
+    return res.send(video);
+    } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+}
+});
+   
+router.delete('/:id', async (req, res) => {
+    try {
+   
+    const video = await Video.findByIdAndRemove(req.params.id);
+    if (!video)
+    return res.status(400).send(`The product with id "${req.params.id}" d
+   oes not exist.`);
+    return res.send(video);
+    } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+
 
 module.exports = router
